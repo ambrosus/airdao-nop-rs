dc46ff982443ef76240e57f798582c076006500c
Eshanchik <kanal33334@gmail.com>
2024-10-28 13:59:17 +0200

    add update node version block

--- update.sh
+++ update.sh
@@ -11,9 +11,24 @@ net.ipv6.conf.all.disable_ipv6=1
 END
 sysctl -p /etc/sysctl.d/10-airdao.conf
 
-git checkout main
-git pull origin main
-git submodule update --init --recursive
+cd ~/airdao-nop-rs
 
-chmod +x run-update.sh
-source run-update.sh
+CURRENT_VERSION=$(./airdao-nop-rs --version | cut -d ' ' -f2)
+LATEST_VERSION=$(curl -s https://raw.githubusercontent.com/ambrosus/airdao-nop-rs/main/Cargo.toml | grep '^version' | sed -E 's/version = "(.*)"/\1/')
+
+if [[ "$CURRENT_VERSION" != "$LATEST_VERSION" ]]; then    
+    DEBIAN_VERSION=$(lsb_release -sr | cut -d '.' -f 1)
+    UBUNTU_VERSION=$(lsb_release -sr | cut -d '.' -f 1)
+    if (( DEBIAN_VERSION > 11 )) || (( UBUNTU_VERSION >= 22 )); then
+        FILE_URL="https://github.com/ambrosus/airdao-nop-rs/releases/download/v$LATEST_VERSION/airdao-nop-rs-x86-64.zip"
+    else
+        FILE_URL="https://github.com/ambrosus/airdao-nop-rs/releases/download/v$LATEST_VERSION/airdao-nop-rs-x86-64-old.zip"
+    fi
+
+    curl -L -o airdao-nop-rs.zip "$FILE_URL"
+    unzip -o airdao-nop-rs.zip
+    chmod +x ./airdao-nop-rs
+
+    ./airdao-nop-rs update
+else
+fi
