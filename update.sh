8a1dc9b1b2665566f5b5c4dcc53a6038b2c4ee1d
Eshanchik <kanal33334@gmail.com>
2024-10-29 13:24:41 +0200

    Added os version check and function for update

--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 cd "$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )"
 if [[ -d /etc/cron.daily ]]; then
   rm -f /etc/cron.daily/airdao-nop-rs
@@ -12,12 +11,25 @@ END
 sysctl -p /etc/sysctl.d/10-airdao.conf
 
 cd ~/airdao-nop-rs || return
-rm airdao-nop-rs
-
-git pull origin main
 
 CURRENT_VERSION=$(./airdao-nop-rs --version | cut -d ' ' -f2)
 LATEST_VERSION=$(curl -s https://raw.githubusercontent.com/ambrosus/airdao-nop-rs/main/Cargo.toml | grep '^version' | sed -E 's/version = "(.*)"/\1/')
+DIR_NAME="airdao-nop-rs"
+ZIP_FILE="${DIR_NAME}.zip"
+
+install_airdao() {
+    cd ~
+    
+    curl -L -o "$ZIP_FILE" "$FILE_URL"
+    echo "Распаковка $ZIP_FILE..."
+    unzip -o "$ZIP_FILE"
+    rm "$ZIP_FILE"
+    
+    cd "$DIR_NAME"
+    chmod +x ./airdao-nop-rs
+    
+    ./airdao-nop-rs update
+}
 
 if [[ "$CURRENT_VERSION" != "$LATEST_VERSION" ]]; then    
     DEBIAN_VERSION=$(lsb_release -sr | cut -d '.' -f 1)
@@ -28,10 +40,5 @@ if [[ "$CURRENT_VERSION" != "$LATEST_VERSION" ]]; then
         FILE_URL="https://github.com/ambrosus/airdao-nop-rs/releases/download/v$LATEST_VERSION/airdao-nop-rs-x86-64-old.zip"
     fi
 
-    curl -L -o airdao-nop-rs.zip "$FILE_URL"
-    unzip -o airdao-nop-rs.zip
-    chmod +x ./airdao-nop-rs
-
-    ./airdao-nop-rs update
-else
-fi
+    install_airdao
+  fi
