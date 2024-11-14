96eb623b3c6f28f2b820a16306218bb8e5afa832
Eshanchik <kanal33334@gmail.com>
2024-11-14 12:52:47 +0200

    add cosign verification and update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,52 @@ apt-get install -y \
     jq \
     unzip
 
+# Install cosign 
+curl -sSL https://github.com/sigstore/cosign/releases/latest/download/cosign-linux-amd64 -o /usr/local/bin/cosign
+chmod +x /usr/local/bin/cosign
+
+LATEST_TAG=$(curl -s https://raw.githubusercontent.com/ambrosus/airdao-nop-rs/main/Cargo.toml | grep '^version' | sed -E 's/version = "(.*)"/\1/')
+DISTRO_NAME=$(lsb_release -i | cut -d ':' -f 2 | xargs)
+MAJOR_VERSION=$(lsb_release -sr | cut -d '.' -f 1)
+
+echo "Distribution: $DISTRO_NAME"
+echo "Major version: $MAJOR_VERSION"
+
+if [[ "$DISTRO_NAME" == "Ubuntu" ]]; then
+    if (( MAJOR_VERSION >= 22 )); then
+        echo "Selecting new binary for Ubuntu"
+        FILE_URL="https://github.com/ambrosus/airdao-nop-rs/releases/download/v$LATEST_TAG/airdao-nop-rs-x86-64.zip"
+    else
+        echo "Selecting old binary for Ubuntu"
+        FILE_URL="https://github.com/ambrosus/airdao-nop-rs/releases/download/v$LATEST_TAG/airdao-nop-rs-x86-64-old.zip"
+    fi
+elif [[ "$DISTRO_NAME" == "Debian" ]]; then
+    if (( MAJOR_VERSION > 11 )); then
+        echo "Selecting new binary for Debian"
+        FILE_URL="https://github.com/ambrosus/airdao-nop-rs/releases/download/v$LATEST_TAG/airdao-nop-rs-x86-64.zip"
+    else
+        echo "Selecting old binary for Debian"
+        FILE_URL="https://github.com/ambrosus/airdao-nop-rs/releases/download/v$LATEST_TAG/airdao-nop-rs-x86-64-old.zip"
+    fi
+else
+    echo "Unknown distribution: $DISTRO_NAME"
+fi
+
+echo "Downloading from URL: $FILE_URL"
+
+curl -L -o airdao-nop-release.zip "$FILE_URL"
+unzip airdao-nop-release.zip
+rm airdao-nop-release.zip
+cd airdao-nop-rs || return
+
+if cosign verify-blob --key airdao-nop-rs.pub --signature airdao-nop-rs.sig airdao-nop-rs; then
+    echo -e "\033[0;32mVerified OK\033[0m"
+else
+    rm -rf ~/airdao-nop-rs 
+    echo -e "\033[0;31mError: Verification failed\033[0m"
+    exit 1
+fi
+
 if [ -f /etc/debian_version ]; then
     DISTRO=$(lsb_release -is)
     if [[ "$DISTRO" == "Debian" ]]; then
@@ -59,22 +105,6 @@ fi
 
 # Revert /etc/needrestart/needrestart.conf to original state after installing required packages
 sed -i 's/^\$nrconf{restart} = '\''a'\'';/$nrconf{restart} = '\''i'\'';/' /etc/needrestart/needrestart.conf
-
-LATEST_TAG=$(curl -s https://raw.githubusercontent.com/ambrosus/airdao-nop-rs/main/Cargo.toml | grep '^version' | sed -E 's/version = "(.*)"/\1/')
-UBUNTU_MAJOR_VERSION=$(lsb_release -sr | cut -d '.' -f 1)
-DEBIAN_MAJOR_VERSION=$(lsb_release -sr | cut -d '.' -f 1)
-
-if (( DEBIAN_MAJOR_VERSION > 11 )) || (( UBUNTU_MAJOR_VERSION >= 22 )); then
-    FILE_URL="https://github.com/ambrosus/airdao-nop-rs/releases/download/v$LATEST_TAG/airdao-nop-rs-x86-64.zip"
-else
-    FILE_URL="https://github.com/ambrosus/airdao-nop-rs/releases/download/v$LATEST_TAG/airdao-nop-rs-x86-64-old.zip"
-fi
-
-curl -L -o airdao-nop-release.zip "$FILE_URL"
-unzip airdao-nop-release.zip
-rm airdao-nop-release.zip
-
-cd airdao-nop-rs || return
 
 chmod +x update.sh
 ./update.sh
